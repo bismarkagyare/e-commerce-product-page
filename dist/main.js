0b77b8a7b82b6c2ad89e6f3579f22c77f1649168
Bismark Agyare <bismarkagyare54@gmail.com>
2023-05-21 20:58:28 +0000

    bundled code

--- dist/main.js
+++ dist/main.js
@@ -3,6 +3,8 @@ var __webpack_exports__ = {};
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
+console.log('hello');
 
 /******/ })()
 ;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vZS1jb21tZXJjZS1wYWdlLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCdoZWxsbycpO1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
