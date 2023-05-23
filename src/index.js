3800a13f0fe60bfad267b9c5475724e7973f5a0f
Bismark Agyare <bismarkagyare54@gmail.com>
2023-05-23 21:50:58 +0000

    increase counter on click event

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 const decreaseBtn = document.querySelector('.minus');
 const increaseBtn = document.querySelector('.plus');
+const digit = document.querySelector('.select-items p');
 const addBtn = document.querySelector('.add-to-cart img');
+
+let count = 0;
+increaseBtn.addEventListener('click', () => {
+  count++;
+  digit.textContent = count;
+});
